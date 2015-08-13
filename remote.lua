5992976ce062da0b913d9097542069a534b8974e
Travis Traber <Traberjkt@gmail.com>
2015-08-12 22:08:00 -0700

    No DisplayFusion -> Replaced with Windows Binds &  Updated UI
    Removed DisplayFusion binds
    
    Added Windows Shortcuts for All previous supported buttons
    
    Updated UI full remote needs theme/color

--- remote.lua
+++ remote.lua
@@ -8,24 +8,66 @@ local kb = libs.keyboard;
 -- http://www.unifiedremote.com/api/libs/keyboard
 
 
-actions.nextwindow = function ()
-	kb.stroke("ctrl", "win", "X");
+actions.nextwindow = function()
+	kb.stroke("win", "shift", "left");
+end
+
+actions.previouswindow = function()
+	kb.stroke("win", "shift", "right");
 end
 
 actions.maximize = function ()
-	kb.stroke("ctrl", "win", "M");
+	kb.stroke("win", "shift", "M");
 end
 
 actions.minimize = function ()
-	kb.stroke("ctrl", "win", "N");
+	kb.stroke("win", "M");
+end
+
+actions.cs = function()
+	kb.stroke("alt", "esc");
+end
+
+actions.nvd = function()
+	kb.stroke("win", "ctrl", "D");
+end
+
+actions.dl = function()
+	kb.stroke("win", "ctrl", "left");
+end
+
+actions.dr = function()
+	kb.stroke("win", "ctrl", "right");
+end
+
+actions.cvd = function()
+	kb.stroke("win", "ctrl", "F4");
+end
+
+actions.close = function()
+	kb.stroke("alt", "F4");
+end
+
+actions.scrollup = function()
+	kb.stroke("up");
+end
+
+actions.scrolldown = function()
+	kb.stroke("down");
+end
+
+actions.zoomin = function()
+	kb.stroke("win", "+");
+end
+
+actions.zoomout = function()
+	kb.stroke("win", "-");
 end
 
 actions.desktop = function ()
 	kb.stroke("win", "D");
 end
 
-actions.windowselect = function ()
+actions.ws = function ()
 	kb.stroke("win", "tab");
 end
-
-
